c9735aa6198191bf2fec54251eaecf7b759811c1
Ruchika-6004 <ruchika6004@gmail.com>
2026-07-18 11:31:23 +0530

    feat: integrate contact email service

--- backend/src/controllers/contact.controller.js
+++ backend/src/controllers/contact.controller.js
@@ -2,15 +2,41 @@ import Contact from "../models/contact.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiHandler.js";
 
+import { sendEmail } from "../services/email.service.js";
+
+import adminEmailTemplate from "../templates/adminEmail.template.js";
+import autoReplyTemplate from "../templates/autoReply.template.js";
+
 export const submitContactForm = asyncHandler(async (req, res) => {
     const { name, email, company, subject, message } = req.body;
 
+    // Save in MongoDB
     const contact = await Contact.create({
         name,
         email,
         company,
         subject,
         message,
+    });
+
+    // Email to Admin
+    await sendEmail({
+        to: process.env.ADMIN_EMAIL,
+        subject: `New Contact Form - ${subject}`,
+        html: adminEmailTemplate({
+            name,
+            email,
+            company,
+            subject,
+            message,
+        }),
+    });
+
+    // Auto Reply to User
+    await sendEmail({
+        to: email,
+        subject: "We've received your message",
+        html: autoReplyTemplate(name),
     });
 
     return res.status(201).json(
--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -2,19 +2,11 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendEmail = async ({ to, subject, html }) => {
-    try {
-        const response = await resend.emails.send({
-            from: process.env.ADMIN_EMAIL,
-            to,
-            subject,
-            html,
-        });
-
-        return response;
-    } catch (error) {
-        throw error;
-    }
+export const sendEmail = async ({ to, subject, html }) => {
+    return await resend.emails.send({
+        from: process.env.ADMIN_EMAIL,
+        to,
+        subject,
+        html,
+    });
 };
-
-export default sendEmail;
